65e0e9366555bf1966792f020df6bedab879d436
Shazid <shazid.dev@gmail.com>
2023-12-03 19:52:08 +0600

    ci: dotenv added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./router";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -25,10 +27,8 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
 
-const MONGO_URL =
-  "mongodb+srv://shaz1d:4nat14xuHvp6UDxD@cluster0.xs8duvx.mongodb.net/?retryWrites=true&w=majority";
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
