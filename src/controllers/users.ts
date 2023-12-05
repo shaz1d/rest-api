231aba82878cfeacecf791c2e1fa569a9fb78d45
Shazid <shazid.dev@gmail.com>
2023-12-05 18:42:36 +0600

    feat: update user controller add

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -25,6 +25,28 @@ export const deleteUser = async (
     const deleteUser = await deleteUserById(id);
 
     return res.json(deleteUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
