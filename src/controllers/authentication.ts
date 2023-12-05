ce48cca54fd689ec606ec5d1a46bcc32c5404ed4
Shazid <shazid.dev@gmail.com>
2023-12-05 17:22:37 +0600

    feat: login controller add

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -9,10 +9,32 @@ export const login = async (req: express.Request, res: express.Response) => {
       return res.sendStatus(400);
     }
 
-    const user = await getUserByEmail(email);
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
     if (!user) {
       return res.sendStatus(400);
     }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("SHAZ1D", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
